50f5cf85eea7351432a51798caeb4c4eb8aaffb7
Alexei <leshamihaylenko@bk.ru>
2024-05-20 00:46:27 +0700

    release python

--- python/4th_ex/common.py
+++ python/4th_ex/common.py
@@ -1,11 +1,31 @@
 from random import randint
+import math
 
 sieve_size = 10000
 
+def is_prime(num):
+    if num <= 1 or num % 2 == 0 or num % 3 == 0:
+        return False
+    if num <= 3:
+        return True
+
+    for i in range(5, int(math.sqrt(num)), 6):
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+    return True
+
+
 def mod_pow(a, x, p):
     res = 1
+    a = a % p
+    if a == 0:
+        return a
+
+    if is_prime(p):
+        x = x % (p - 1)
+
     while x > 0:
-        if x % 2 == 1:
+        if x & 1:
             res = res * a % p
         a = a * a % p
         x >>= 1
