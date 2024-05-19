63badad4548170b0adfb92c43744dbf8b98ee96e
Alexei <leshamihaylenko@bk.ru>
2024-05-20 00:54:07 +0700

    release C++

--- C++/5th_ex/main.cpp
+++ C++/5th_ex/main.cpp
@@ -1,7 +1,9 @@
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
+bool is_prime(int num);
 int mod_pow(int a, int x, int p);
 
 int main() {
@@ -17,9 +19,33 @@ int main() {
   return 0;
 }
 
+bool is_prime(int num) {
+  if (num <= 1 || num % 2 == 0 || num % 3 == 0)
+    return false;
+  if (num <= 3)
+    return true;
+
+  for (int i = 5; i < sqrt(num); i += 6)
+    if (num % i == 0 || num % (i + 2) == 0)
+      return false;
+  return true;
+}
+
 int mod_pow(int a, int x, int p) {
+  a = a % p;
+  if (a == 0)
+    return a;
+
+  if (is_prime(p))
+    x = x % (p - 1);
+
   int res = 1;
-  for (int i = 0; i < x; ++i)
-    res = res * a % p;
+
+  while (x > 0) {
+    if (x & 1)
+      res = res * a % p;
+    a = a * a % p;
+    x >>= 1;
+  }
   return res;
 }
