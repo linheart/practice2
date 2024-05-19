36472f9585863de6ceaf14c3dededa38165b3a76
Alexei <leshamihaylenko@bk.ru>
2024-05-20 00:55:53 +0700

    release python

--- python/5th_ex/main.py
+++ python/5th_ex/main.py
@@ -1,7 +1,30 @@
+import math
+
+def is_prime(num):
+    if num <= 1 or num % 2 == 0 or num % 3 == 0:
+        return False
+    if num <= 3:
+        return True
+
+    for i in range(5, int(math.sqrt(num)), 6):
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+    return True
+
 def mod_pow(a, x, p):
     res = 1
-    for _ in range(x):
-        res = res * a % p
+    a = a % p
+    if a == 0:
+        return a
+
+    if is_prime(p):
+        x = x % (p - 1)
+
+    while x > 0:
+        if x & 1:
+            res = res * a % p
+        a = a * a % p
+        x >>= 1
     return res
 
 if __name__ == '__main__':
